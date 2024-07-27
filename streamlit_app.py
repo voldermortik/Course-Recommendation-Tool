14b3a02b233afba5acfab30e0399a4be45d8e1c5
voldermortik <utkarsh.raj@interviewkickstart.com>
2024-07-28 03:22:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Function to read HTML file
+def load_html(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+# Load HTML content
+html_string = load_html('https://github.com/voldermortik/Course-Selection-Tool/blob/main/course_selection.html')  
+# Replace 'index.html' with the path to your HTML file
+
+# Embed HTML content in Streamlit app
+components.html(html_string, height=800, scrolling=True)
