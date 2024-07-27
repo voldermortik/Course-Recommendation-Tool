6bffc3aa73c5111f1b436be9cd4a4936545a8233
voldermortik <utkarsh.raj@interviewkickstart.com>
2024-07-28 03:38:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ import streamlit.components.v1 as components
 
 # Function to read HTML file
 def load_html(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
 # Load HTML content
 html_string = load_html('course_selection.html')  
-# Replace 'index.html' with the path to your HTML file
+# Replace 'course_selection.html' with the path to your HTML file
 
-# Embed HTML content in Streamlit app
-components.html(html_string, height=1000, width=800, scrolling=True)
+# Embed HTML content in Streamlit app with dynamic height
+components.html(html_string, height=st.sidebar.slider("Height", 200, 1500, 800), scrolling=True)
